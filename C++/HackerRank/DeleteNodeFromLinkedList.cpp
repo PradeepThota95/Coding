f8c8f000f5f84dcc1d5ef5e89903292c9da48119
PradeepThota95 <pradeepthota95@gmail.com>
2019-10-16 22:51:29 +0530

    Delete a node at given position in Linked list DeleteNodeFromLinkedList.cpp

---
+++ C++/HackerRank/DeleteNodeFromLinkedList.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the deleteNode function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head, int position) {
+    
+    SinglyLinkedListNode* p = head, *q = NULL;
+    if(position == 0) {
+        if (head)
+        {
+            p = head->next;
+            head->next = NULL;
+            free(head);
+            head = p; 
+        }
+        return head;
+    }
+
+    while(position --)
+    {
+        if(p)
+        {
+            q = p;
+	    cout << "data : " << p->data << endl;
+            p = p->next;
+        }
+         
+    }
+
+    q->next = (p->next);
+    p->next = NULL;
+    
+    free(p);
+    return head;
+}
+
+void print_list( SinglyLinkedListNode* head)
+{
+	 SinglyLinkedListNode* p= head;
+	if(!head)
+		cout << "The head of the list is NUll returning " << endl;
+	while(p)
+	{
+		cout << p->data << " " ;
+		p = p->next;
+	}
+}
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    SinglyLinkedList* llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist->insert_node(llist_item);
+    }
+
+    int position;
+    cin >> position;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    SinglyLinkedListNode* llist1 = deleteNode(llist->head, position);
+
+    print_list(llist->head);
+    print_singly_linked_list(llist1, " ", fout);
+    fout << "\n";
+
+    free_singly_linked_list(llist1);
+
+    fout.close();
+
+    return 0;
+}
+
